af01ae50d829e343f884f247e08d7fecc94f1036
Johann Westphall <johannwestphall@gmail.com>
2025-11-06 17:17:13 -0300

    Fix suggested by review

--- backends/edge.py
+++ backends/edge.py
@@ -79,7 +79,7 @@ def listen_to_stdin():
             if send_to_file:
                 asyncio.run(save_to_file(text))
             else:
-                ex.submit(asyncio.run, stream_audio(text))
+                ex.submit(lambda: asyncio.run(stream_audio(text)))
             text = ""
         text += character
 
